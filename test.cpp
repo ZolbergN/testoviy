674ea2a4b84f0393de8fa5db637b1afa9c386934
Roman <exculibur@rambler.ru>
2017-12-04 23:26:12 +0300

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class XXX
+    {
+    public:
+    void fun1()
+    {
+    cout « "hi\n";
+    }
+    private:
+    void fun2()
+    {
+    cout « "won't be printed";
+    }
+    protected:
+    void fun3()
+    {
+    cout « "won't be printed";
+    }
+    };
+
+    int main()
+    {
+    XXX obj;
+    obj.fun1();
+
+    //obj.fun2();//error: ‘void XXX::fun2()’ is private
+
+       //obj.fun3();//error: ‘void XXX::fun3()’ is protected
+
+ return 0;
+}

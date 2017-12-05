1ae0e07847d01efaf24d2e492d51b912f9915166
Roman <exculibur@rambler.ru>
2017-12-05 16:00:37 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,33 +2,12 @@
 
 using namespace std;
 
-class XXX
-    {
-    public:
-    void fun1()
-    {
-    cout << "hi\n";
-    }
-    private:
-    void fun2()
-    {
-    cout << "won't be printed";
-    }
-    protected:
-    void fun3()
-    {
-    cout << "won't be printed";
-    }
-    };
-
-    int main()
-    {
-    XXX obj;
-    obj.fun1();
-
-    //obj.fun2();//error: ‘void XXX::fun2()’ is private
-
-       //obj.fun3();//error: ‘void XXX::fun3()’ is protected
-
- return 0;
+int main()
+{
+    int a = 20;
+    int b = 13;
+    int sum = a + b;
+    cout << "sum= " << sum << endl;
+    
+    return 0;
 }
